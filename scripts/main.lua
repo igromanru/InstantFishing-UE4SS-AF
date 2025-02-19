b5a8ad4f5ed1fdfb4cbc2a2c0d415a27b7d07a64
igromanru <igromanru@yahoo.de>
2025-02-19 21:45:33 +0100

    feat: Infinite Bait configuration (default: false)

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,12 +4,17 @@
     Mod Name: Instant Fishing
 ]]
 
+---------- Configurations ----------
+InfiniteBait = false
+-------------------------------------
+
 ------------------------------
 -- Don't change code below --
 ------------------------------
 local ModName = "InstantFishing"
-local ModVersion = "1.1.2"
+local ModVersion = "1.2.0"
 local DebugMode = true
+local IsModEnabled = true
 
 local function ModInfoAsPrefix()
     return "["..ModName.." v"..ModVersion.."] "
@@ -17,18 +22,19 @@ end
 
 print(ModInfoAsPrefix().."Starting mod initialization\n")
 
-local IsModEnabled = true
-
 local function StartFishingMinigameHook(Context)
     local fishingRod = Context:get() ---@type AWeapon_FishingRod_C
 
     -- if DebugMode then
     --     print(ModInfoAsPrefix().."---- [Start Fishing Minigame] called ----\n")
+    --     print(ModInfoAsPrefix()..string.format("CatchingJunk: %s\n", tostring(fishingRod.CatchingJunk)))
     --     print(ModInfoAsPrefix()..string.format("LuckyHat: %s\n", tostring(fishingRod.LuckyHat)))
     --     print(ModInfoAsPrefix().."------------------------------\n")
     -- end
     if IsModEnabled then
-        -- fishingRod.LuckyHat = true
+        if not InfiniteBait then
+            fishingRod:Request_TriggerBaitUsage()
+        end
         fishingRod:FishingSuccess()
     end
 end
